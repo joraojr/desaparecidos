dd06b69bdd2399e2f3c20704f32216872a573894
Ramon Oliveira <ramonomello@gmail.com>
2018-05-22 14:57:17 -0300

    bairronanet.com.br faltando paginação e tratar duplicatas

--- scripts/raspagem/[incompleto]bairronanet.com.br.php
+++ scripts/raspagem/[incompleto]bairronanet.com.br.php
@@ -4,41 +4,33 @@ include("../simple_html_dom/simple_html_dom.php");
 include("atualizacaoPrincipal.php");
 
 
-for($i = 1; $i <= 1; $i++){
-    $page = "http://bairronanet.com.br/desaparecidos/pagina_principal_desaparecidos.php?sit=%&&sexo=%&&olhos=%&&oculos=%&&comp=%&&barba=%&&cabelo=%&&tipo=%&&pais=%&&estado=%&&cidade=%&&bairro=%&&nome=%&&pai=%&&mae=%&&pele=%&&marcas=&&img=&&np=12&&paginaatual=1#topodapesquisa";
+for($k = 1; $k <= 5; $k++){
+    $page = "http://bairronanet.com.br/desaparecidos/pagina_principal_desaparecidos.php?sit=%&&sexo=%&&olhos=%&&oculos=%&&comp=%&&barba=%&&cabelo=%&&tipo=%&&pais=%&&estado=%&&cidade=%&&bairro=%&&nome=%&&pai=%&&mae=%&&pele=%&&marcas=&&img=&&np=12&&paginaatual=$k#topodapesquisa";
     $html = file_get_html($page);
     $href = "http://bairronanet.com.br/desaparecidos/";
 
-    foreach ($html -> find('li span.MarromNomeProduto12 text') as $people){
+    foreach ($html -> find('li span.MarromNomeProduto12 text') as $people) {
         $link = $href . "desaparecidos_mostra.php?cod={$people->_[4]}&linkdeacesso=pagina-pessoas-desaparecidas-btmaisinfo";
         $page_people = file_get_html($link);
 
+
         $data = array();
-//        $data["Fonte"] = $link;
-//        $data["Foto"] = $href . $page_people -> find('table[cellpadding=3] img', 0)->src;
-//        $data["Nome"] = $page_people -> find('table[cellpadding=3] span.ClassMarromEscuro14', 0)->plaintext;
-//        $data["Situacao"] = $page_people -> find('table[cellpadding=3] span.ClassMarromEscuro14', 1)->plaintext;
-//        $data["Contato"] = $page_people -> find('table[cellpadding=3] span.ClassMarromEscuro14', 2)->plaintext;
-//
-        //}
+        $data["Fonte"] = $link;
+        $data["Foto"] = $href . $page_people->find('table[cellpadding=3] img', 0)->src;
 
-        foreach ($page_people -> find('table[cellpadding=3]') as $inf) {
-            $info = str_get_html($inf);
-            $data["Nome"] = $info->find('span.ClassMarromEscuro14 text')->plaintext;
-            $data["Situacao"] = $info->find('span.ClassMarromEscuro14 text')->plaintext;
-            $data["Telefone"] = $info->find('span.ClassMarromEscuro14 text')->plaintext;
-            print_r($inf);
-        }
+        $data["Nome"] = $page_people->find('table[cellpadding=3] span.ClassMarromEscuro14', 0)->plaintext;
+        $data["Situacao"] = trim($page_people->find('table[cellpadding=3] span.ClassMarromEscuro14', 1)->plaintext);
+        $data["Contato"] = trim($page_people->find('table[cellpadding=3] span.ClassMarromEscuro14', 2)->plaintext);
 
-//            foreach ($info->find('span.ClassMarromEscuro14 text') as $inf) {
-//                $p = str_get_html($inf);
-//                $data["Nome"] = $p->plaintext;
-//                $data["Situacao"] = $p->plaintext;
-//                $data["Telefone"] = $p->plaintext;
-//                echo $inf->_[4] . "\n";
-//            }
-        var_dump($data);
+        for ($i = 0, $j = 0; $j<31, $i<32; $j++, $i++) {
+            if($i == 1) {
+                $i = $i+1;
+                $data[trim($page_people->find('tr td.ClassCorpoTextoNormal13pt', $i)->plaintext)] = trim($page_people->find('tr td div[align="justify"]', $j)->plaintext);
+                continue;
+            }
+            $data[trim($page_people->find('tr td.ClassCorpoTextoNormal13pt', $i)->plaintext)] = trim($page_people->find('tr td div[align="justify"]', $j)->plaintext);
         }
-    //}
+        print_r($data);
+    }
 }
 
